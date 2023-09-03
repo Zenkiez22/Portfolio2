7cce8b0ce743e996245cfcbc3a67529b039565fb
Zenkiez22 <96461012+Zenkiez22@users.noreply.github.com>
2023-09-03 22:59:39 +0800

    update codes

--- script.js
+++ script.js
@@ -1,37 +1,16 @@
-let words = document.querySelectorAll(".word");
-words.forEach((word) => {
-    let letters = word.textContent.split("");
-    word.textContent = "";
-    letters.forEach((letter) => {
-        let span = document.createElement("span");
-        span.textContent = letter;
-        span.className = "letter";
-        word.append(span);
-    });
-});
+const text = document.querySelector(".seco-text");
 
-let currentWordIndex = 0;
-let maxWordIndex = words.length - 1;
-words[currentWordIndex].style.opacity = "1";
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = "Fullstack Web Developer";
+    }, 0);
+    setTimeout(() => {
+        text.textContent = "UX/UI Designer";
+    }, 4000);
+    setTimeout(() => {
+        text.textContent = "Freelancer";
+    }, 8000);
+}
 
-let changeText = () => {
-    let currentWord = words[currentWordIndex];
-    let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
-
-    Array.from(currentWord.children).forEach((letter, i) => {
-        setTimeout(() => {
-            letter.className = "letter out";
-        }, i * 80);
-    });
-    nextWord.style.opacity = "1";
-    Array.from(nextWord.children).forEach((letter, i) => {
-        letter.className = "letter behind";
-        setTimeout(() => {
-            letter.className = "letter in";
-        }, 340 + i * 80);
-    });
-    currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
-};
-
-changeText();
-setInterval(changeText, 3000);
+textLoad();
+setInterval(textLoad, 12000);
