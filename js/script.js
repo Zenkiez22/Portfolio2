d3d049dee3ffdc96b1ed43a37875bc5f394a0839
Zenkiez22 <96461012+Zenkiez22@users.noreply.github.com>
2023-09-18 22:22:55 +0800

    adding scroll-scale and scroll-top , and scroll-bottom in every section

--- js/script.js
+++ js/script.js
@@ -1,19 +1,36 @@
 
 // Active Menu 
 
-let menuLi = document.querySelectorAll('header ul li a');
 let section = document.querySelectorAll('section');
+let menuLi = document.querySelectorAll('header ul li a ');
 
-function activeMenu() {
-    let len = section.length;
-    while (--len && window.scrollY + 97 < section[len].offsetTop) {
-    }
-    menuLi.forEach(sec => sec.classList.remove("active"));
-    menuLi[len].classList.add("active");
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            menuLi.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header ul li a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
 }
 
-activeMenu();
-window.addEventListener("scroll", activeMenu);
+// function activeMenu() {
+//     let len = section.length;
+//     while (--len && window.scrollY + 97 < section[len].offsetTop) {
+//     }
+//     menuLi.forEach(sec => sec.classList.remove("active"));
+//     menuLi[len].classList.add("active");
+// }
+
+// activeMenu();
+// window.addEventListener("scroll", activeMenu);
 
 
 
@@ -37,7 +54,7 @@ setInterval(textLoad, 12000);
 const header = document.querySelector("header");
 window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", window.scrollY > 50);
-})
+});
 
 // toggle icon navbar
 let menuIcon = document.querySelector("#menu-icon");
@@ -48,7 +65,29 @@ menuIcon.onclick = () => {
     navlist.classList.toggle("open");
 }
 
-window.onscroll = () =>{
+window.onscroll = () => {
     menuIcon.classList.remove("bx-x");
     navlist.classList.remove("open");
 }
+
+// parallax
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show-items");
+        }
+        else {
+            entry.target.classList.add("show-items");
+        }
+
+    });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el) => observer.observe(el));
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el) => observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el) => observer.observe(el));
